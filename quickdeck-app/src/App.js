5f460db80f08351fed0745aee86b5ff75daddbef
Oluwajoba <oluwajobabello@gmail.com>
2019-12-14 04:04:02 +0100

    install husky succesfully6

--- quickdeck-app/src/App.js
+++ quickdeck-app/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-// "extends": [
-//   "eslint:recommended",
-//   "plugin:react/recommended",
-//   "prettier", "prettier/react"
-// ],
+
 function App() {
   return (
     <div className="App">
