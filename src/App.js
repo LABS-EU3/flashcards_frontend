01b4a6c0e9b68d4c92bf46211333658d3744747a
richanynguon <alina.nguon@gmail.com>
2019-12-17 11:15:48 -0500

    complete background set up for sign up and login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './App.css';
-import { H1, H3 } from './styles/typography';
-import { Button, LineButton } from './styles/buttons';
-import { ParentBackground, SkewDiv, UnSkewDiv } from './styles/background';
 
 function App() {
   return (
-    <div>
-      <ParentBackground>
-        <SkewDiv>
-          <UnSkewDiv>
-            <H1>Welcome to Quick Decks!</H1>
-            <Button>
-              <H3 WHITE>Login</H3>
-            </Button>
-            <LineButton>
-              <H3 PRIMARY>Login</H3>
-            </LineButton>
-          </UnSkewDiv>
-        </SkewDiv>
-      </ParentBackground>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Quick Decks!</h1>
+      </header>
     </div>
   );
 }
--- src/styles/background.js
+++ src/styles/background.js
@@ -6,7 +6,37 @@ import styled from 'styled-components';
 // Styles
 import * as c from './variables/colours';
 
+// Assests
+import topTriangle from '../assets/topTriangle.svg';
+import bottomTriangle from '../assets/bottomTriangle.svg';
+
+// To use this background please follow this format
+/*
+
+     <ParentBackground>
+        <TopTriangle />
+        <SkewDiv>
+          <UnSkewDiv>
+
+            <H1>Welcome to Quick Decks!</H1>
+            <Button>
+              <H3 WHITE>Login</H3>
+            </Button>
+            <LineButton>
+              <H3 PRIMARY>Login</H3>
+            </LineButton>
+            
+          </UnSkewDiv>
+        </SkewDiv>
+        <BottomTriangle />
+      </ParentBackground>
+
+*/
+
 export const ParentBackground = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   background: ${c.LIGHT_NEUTRAL_COLOR};
   min-width: 320px;
   min-height: 524px;
@@ -16,6 +46,9 @@ export const ParentBackground = styled.div`
 `;
 
 export const SkewDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #ffffff;
   -webkit-transform: skewY(-16.27deg);
   -moz-transform: skewY(-16.27deg);
@@ -23,6 +56,7 @@ export const SkewDiv = styled.div`
   -o-transform: skewY(-16.27deg);
   transform: skewY(-16.27deg);
   z-index: 1;
+  height: 75vh;
 `;
 
 export const UnSkewDiv = styled.div`
@@ -32,4 +66,24 @@ export const UnSkewDiv = styled.div`
   -o-transform: skewY(16.27deg);
   transform: skewY(16.27deg);
   z-index: 2;
+`;
+
+export const TopTriangle = styled.div`
+  background-image: url(${topTriangle});
+  background-repeat: no-repeat;
+  background-size: cover;
+  content: '';
+  width: 50%;
+  height: 20vh;
+  align-self: flex-end;
+`;
+
+export const BottomTriangle = styled.div`
+  background-image: url(${bottomTriangle});
+  background-repeat: no-repeat;
+  background-size: cover;
+  content: '';
+  width: 50%;
+  height: 30vh;
+  align-self: flex-start;
 `;
