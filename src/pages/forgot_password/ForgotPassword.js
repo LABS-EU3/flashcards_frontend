42e07e325b4f01804c849ddaf7558bdf76bae802
richanynguon <alina.nguon@gmail.com>
2019-12-18 08:36:47 -0500

    intial commit

--- src/pages/forgot_password/ForgotPassword.js
+++ src/pages/forgot_password/ForgotPassword.js
@@ -9,7 +9,7 @@ import { H1 } from '../../styles/typography';
 export default function ForgotPassword() {
   return (
     <div>
-      <H1>ForgotPassword Test</H1>
+      <H1>Intial Commit</H1>
     </div>
   );
 }
