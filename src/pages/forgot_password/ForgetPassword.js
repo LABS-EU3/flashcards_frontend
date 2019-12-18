0aa148f33d4917ae08ad972aef0cab5e86313f69
richanynguon <alina.nguon@gmail.com>
2019-12-18 04:36:42 -0500

    create forgetpassword.js boiler

---
+++ src/pages/forgot_password/ForgetPassword.js
@@ -0,0 +1,15 @@
+// Import
+
+// Libraries
+import React from 'react';
+
+// Styled
+import { H1 } from '../../styles/typography';
+
+export default function ForgetPassword() {
+  return (
+    <div>
+      <H1>ForgetPassword Test</H1>
+    </div>
+  );
+}
