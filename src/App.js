e9781f08131c9e842ad0e4073678c8c9941d0d3e
richanynguon <alina.nguon@gmail.com>
2019-12-18 05:49:58 -0500

    test pushing

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Login from './pages/login/Login';
 function App() {
   return (
     <div>
+      Test
       <TopBar />
       <Route exact path="/" component={Dashboard} />
       <Route path="/landing" component={Landing} />
