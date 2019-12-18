fb4f8091e3f77a47eef7effd5bdbe3ea6be9f6e7
richanynguon <alina.nguon@gmail.com>
2019-12-18 04:29:18 -0500

    create browser wrapper for the app in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+// Import
+
+// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+// Styles
 import './index.css';
 import App from './App';
+
+// Redux Store
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root'),
 );
