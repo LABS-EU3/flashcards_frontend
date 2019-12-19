1302bec48b0e0b826455238a4940856bded06e50
Omulosi <mulongojohnpaul@gmail.com>
2019-12-19 11:52:56 +0300

    Create a Private Route component

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,2 +1,26 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem('token')) {
+          return <Component {...props} />;
+        }
+        alert('Hi there 😊, Please Sign in');
+        return (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { referer: props.location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
