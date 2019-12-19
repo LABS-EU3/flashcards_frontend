0cf74ebc55f25d5d4dc5f104be165fddd11ebaa8
richanynguon <alina.nguon@gmail.com>
2019-12-19 13:21:47 -0500

    complete styling for carousel

--- src/components/landingCarousel/swiperAnimation/styles.js
+++ src/components/landingCarousel/swiperAnimation/styles.js
@@ -16,11 +16,11 @@ export const Slider = styled.div`
   flex-direction: 'column';
   align-items: center;
   justify-content: center;
-  width: 100%;
+  height: 100%;
 `;
 
 export const SliderContainer = styled.div`
-  width: 70%;
+  width: 150%;
   margin: 1em 0em 1em 0em;
   overflow: hidden;
 `;
