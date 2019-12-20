bed0ac21e4598ed00cde610041ccfcab804b53cc
richanynguon <alina.nguon@gmail.com>
2019-12-20 11:13:51 -0500

    intial commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const baseUrl = 'https://quickdecks-staging.herokuapp.com/api';
+export const baseUrl = 'https://quickdecks.herokuapp.com/api';
