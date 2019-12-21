bc0e70cf584d3893862d999a14c45542fe3dfbd7
Oluwajoba <oluwajobabello@gmail.com>
2019-12-21 01:21:21 +0100

    remove unneccessary commented code / EmailConfirmation.js

--- src/pages/email_confirmation/EmailConfirmation.js
+++ src/pages/email_confirmation/EmailConfirmation.js
@@ -5,13 +5,9 @@ import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-// import { Formik } from 'formik';
-// import * as Yup from 'yup';
-
 // Styled
 import { H1, P } from '../../styles/typography';
-// import { Button, BackArrowButton } from '../../styles/buttons';
-// import { Form, Input, Label } from '../../styles/forms';
+
 import { FlexColumnSpaceBetween } from '../../styles/displayFlex';
 import {
   BottomTriangle,
@@ -20,19 +16,16 @@ import {
   SkewDiv,
   UnSkewDiv,
   FlexRowBackground,
-  // DesktopImage,
+  DesktopImage,
 } from '../../styles/background';
 
-// assets
-// import QuestionMark from '../../assets/icons/noun_Question_3013473 1.svg';
-// import BackArrow from '../../assets/icons/Arrow 1.svg';
-// import KnowledgeSVG from '../../assets/undraw/undraw_knowledge_g5gf.svg';
+import KnowledgeSVG from '../../assets/images/undraw_knowledge_g5gf.svg';
 
 export default function EmailConfirmation() {
   const history = useHistory();
   const tokenFromUrl = useParams();
-  console.log('ttttttttttttttttt', tokenFromUrl);
-  console.log('historyyyyyyyy', history);
+  console.log('token from url', tokenFromUrl);
+  console.log('history', history);
   axios
     .post(
       `https://quickdecks-staging.herokuapp.com/api/auth/confirm`,
@@ -41,23 +34,23 @@ export default function EmailConfirmation() {
     .then(res => {
       console.log('confirm response', res);
       history.push('/dashboard');
+    })
+    .catch(err => {
+      console.log('error request', err.request);
+      console.log('error response', err.response);
+      return err.response;
     });
 
   return (
     <FlexRowBackground>
-      {/* <DesktopImage>
-        <img src={`${KnowledgeSVG}`} alt="back arrow" />
-      </DesktopImage> */}
+      <DesktopImage>
+        <img src={`${KnowledgeSVG}`} alt="knowledagble person" />{' '}
+      </DesktopImage>
       <ParentBackground>
-        {/* <BackArrowButton>
-          <img src={`${BackArrow}`} alt="back arrow" />
-          <H5>Back</H5>
-        </BackArrowButton> */}
         <TopTriangle />
         <SkewDiv>
           <UnSkewDiv>
             <FlexColumnSpaceBetween>
-              {/* <img src={`${QuestionMark}`} alt="question mark icon" /> */}
               <H1 REGULAR>Welcome to QuickDeck</H1>
               <P LIGHTWEIGHT>Your Email EmailConfirmation was successful! </P>
               <br />
