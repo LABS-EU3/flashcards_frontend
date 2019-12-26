0aae397a2c4d07aa5bdca6ccd98b486962d37328
Oluwajoba <oluwajobabello@gmail.com>
2019-12-26 14:47:39 +0100

    add wait message to page / EmailConfirmation.js

--- src/pages/email_confirmation/EmailConfirmation.js
+++ src/pages/email_confirmation/EmailConfirmation.js
@@ -8,7 +8,7 @@ import { withFormik } from 'formik';
 import { emailConfirmation } from '../../modules/user/userActions';
 
 // Styled
-import { H1, P } from '../../styles/typography';
+import { H1, H3 } from '../../styles/typography';
 
 import { FlexColumnSpaceBetween } from '../../styles/displayFlex';
 import {
@@ -42,7 +42,8 @@ function EmailConfirmation(props) {
           <UnSkewDiv>
             <FlexColumnSpaceBetween>
               <H1 REGULAR>Welcome to QuickDeck</H1>
-              <P LIGHTWEIGHT>Your Email EmailConfirmation was successful! </P>
+              <H3 LIGHTWEIGHT>Your Email Confirmation was successful! </H3>
+              <H3 LIGHTWEIGHT>Kindly wait while we log you in</H3>
               <br />
             </FlexColumnSpaceBetween>
           </UnSkewDiv>
