5e20bb125e2304ad7f0d7dfccad4abf78fca166d
Oluwajoba <oluwajobabello@gmail.com>
2019-12-26 14:19:42 +0100

    add baseUrl backend localhost / config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,10 @@
 // export const baseUrl = 'https://quickdecks-staging.herokuapp.com/api';
 
 // eslint-disable-next-line import/prefer-default-export
-// export const baseUrl = 'http://localhost:4003/api';
+export const baseUrl = 'http://localhost:4003/api';
 
 // eslint-disable-next-line import/prefer-default-export
-export const baseUrl = process.env.REACT_APP_API_HOST;
+// export const baseUrl = process.env.REACT_APP_API_HOST;
 
 // [context.deploy - preview.environment]
 // REACT_APP_API_HOST = "https://quickdecks-staging.herokuapp.com/api"
