0520e8fc19c1e27bb2258f20740e56969bed20b4
richanynguon <alina.nguon@gmail.com>
2020-01-07 06:47:52 -0500

    add and implemented helper functions

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,6 +4,7 @@
 import { Redirect } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
+import decode from 'jwt-decode';
 
 // Configs
 import { baseUrl } from '../config';
@@ -19,8 +20,47 @@ export const axiosWithAuth = () => {
   });
 };
 
+export const isTokenExpired = token => {
+  try {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const clearLocalStorage = () => {
+  localStorage.removeItem('token');
+};
+
+export const getToken = () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      return undefined;
+    }
+    const isExpired = isTokenExpired(token);
+    if (isExpired) {
+      clearLocalStorage();
+      return undefined;
+    }
+    return JSON.parse(token);
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export const decodeToken = () => {
+  const token = getToken();
+  const info = token ? decode(token) : undefined;
+  return info;
+};
+
 export function WithAuthCheck(Component, props) {
-  if (localStorage.getItem('token')) {
+  if (decodeToken() !== undefined) {
     return <Component {...props} />;
   }
   return <Redirect to="/login" />;
