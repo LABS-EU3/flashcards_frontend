f72e0ce9ebf911a5fee50de501b89fe5281f9fdd
Anna Winther <anski.anna@gmail.com>
2020-01-07 17:09:57 +0000

    Initial commit and trying to pass props to Sidebar component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,20 +3,29 @@
 // Libraries
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import { useHistory } from 'react-router';
 // Styled
 import { H1, H3 } from '../../styles/typography';
 
+// components
+import RightSidebar from '../../components/rightsidebar/RightSidebar';
+
 export default function Dashboard() {
+  const history = useHistory();
   return (
     <div>
-      <H1>Dashboard Test</H1>
-      <H3>Welcome! LogIn successful</H3>
-      <NavLink to="/dashboard">dashboard</NavLink>
-      <NavLink to="/forgot">forgot password</NavLink>
-      <NavLink to="/reset">reset password</NavLink>
-      <NavLink to="/signup">signup</NavLink>
-      <NavLink to="/login">login</NavLink>
+      <div>
+        <H1>Dashboard Test</H1>
+        <H3>Welcome! LogIn successful</H3>
+        <NavLink to="/dashboard">dashboard</NavLink>
+        <NavLink to="/forgot">forgot password</NavLink>
+        <NavLink to="/reset">reset password</NavLink>
+        <NavLink to="/signup">signup</NavLink>
+        <NavLink to="/login">login</NavLink>
+      </div>
+      <div>
+        <RightSidebar history={history} />
+      </div>
     </div>
   );
 }
