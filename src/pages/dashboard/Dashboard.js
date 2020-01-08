81a1378e6b7007ac30ec737039050b38ddbf2ff4
Anna Winther <anski.anna@gmail.com>
2020-01-08 10:00:28 +0000

    Add more styling and import React Library

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,30 +2,32 @@
 
 // Libraries
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router';
 // Styled
+// import Sidebar from 'react-sidebar';
 import { H1, H3 } from '../../styles/typography';
+import { FlexRowCenterCenter } from '../../styles/displayFlex';
 
 // components
-import RightSidebar from '../../components/rightsidebar/RightSidebar';
+import RightSidebar from '../../components/rightSidebar/RightSidebar';
 
 export default function Dashboard() {
   const history = useHistory();
   return (
-    <div>
+    <FlexRowCenterCenter>
       <div>
         <H1>Dashboard Test</H1>
         <H3>Welcome! LogIn successful</H3>
-        <NavLink to="/dashboard">dashboard</NavLink>
+        {/* <NavLink to="/dashboard">dashboard</NavLink>
         <NavLink to="/forgot">forgot password</NavLink>
         <NavLink to="/reset">reset password</NavLink>
         <NavLink to="/signup">signup</NavLink>
-        <NavLink to="/login">login</NavLink>
+        <NavLink to="/login">login</NavLink> */}
       </div>
       <div>
         <RightSidebar history={history} />
       </div>
-    </div>
+    </FlexRowCenterCenter>
   );
 }
--- src/styles/displayFlex.js
+++ src/styles/displayFlex.js
@@ -16,3 +16,10 @@ export const FlexColumnCenterCenter = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const FlexRowCenterCenter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
