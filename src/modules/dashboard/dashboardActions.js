9345213cdebaec50900ffcab8b9520207746934a
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-08 14:02:57 +0100

    :construction: Set up redux for dashboard

---
+++ src/modules/dashboard/dashboardActions.js
@@ -0,0 +1,23 @@
+import {
+  ON_BEGIN_PROFILE_FETCH,
+  //   ON_PROFILE_FETCH_FAILED,
+  //   ON_PROFILE_FETCH_SUCCESS,
+} from './dashboardTypes';
+
+import { baseUrl } from '../../config/index';
+
+import { axiosWithAuth } from '../../utils/auth';
+
+// eslint-disable-next-line import/prefer-default-export
+export const fetchProfile = () => dispactch => {
+  dispactch({ type: ON_BEGIN_PROFILE_FETCH });
+
+  axiosWithAuth()
+    .get(`${baseUrl}/somen`)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
