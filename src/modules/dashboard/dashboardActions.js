e23feca6dc887db89e56896596913249684b9699
Anna Winther <anski.anna@gmail.com>
2020-01-10 10:45:47 +0000

    Fix typo and create actions and reducers

--- src/modules/dashboard/dashboardActions.js
+++ src/modules/dashboard/dashboardActions.js
@@ -2,25 +2,48 @@ import {
   ON_BEGIN_PROFILE_FETCH,
   ON_PROFILE_FETCH_FAILED,
   ON_PROFILE_FETCH_SUCCESS,
+  RECENT_CARDS_START,
+  RECENT_CARDS_SUCCESS,
+  RECENT_CARDS_FAILED,
 } from './dashboardTypes';
 
 import { axiosWithAuth } from '../../utils/auth';
 
 // eslint-disable-next-line import/prefer-default-export
-export const fetchProfile = () => dispactch => {
-  dispactch({ type: ON_BEGIN_PROFILE_FETCH });
+export const fetchProfile = () => dispatch => {
+  dispatch({ type: ON_BEGIN_PROFILE_FETCH });
 
   axiosWithAuth()
     .get(`/auth/view_profile`)
     .then(({ data }) => {
-      dispactch({
+      dispatch({
         type: ON_PROFILE_FETCH_SUCCESS,
         payload: data.user,
       });
     })
     .catch(err => {
-      dispactch({
+      dispatch({
         type: ON_PROFILE_FETCH_FAILED,
+        payload: err,
+      });
+    });
+};
+
+export const getRecentCards = userId => dispatch => {
+  dispatch({ type: RECENT_CARDS_START });
+
+  axiosWithAuth()
+    .get(`/api/cards/users/${userId}`)
+    .then(({ data }) => {
+      dispatch({
+        type: RECENT_CARDS_SUCCESS,
+        payload: data.user,
+      });
+      console.log(data.user);
+    })
+    .catch(err => {
+      dispatch({
+        type: RECENT_CARDS_FAILED,
         payload: err,
       });
     });
