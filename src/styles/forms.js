38b3672c3ebf7e7b52b4c6bb91155627c7c31aa2
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-11 16:49:52 +0100

    :art: Improve re-usability of form components
    Add Select component as a reusable component

--- src/styles/forms.js
+++ src/styles/forms.js
@@ -14,6 +14,7 @@ export const Forms = styled.form`
   flex-direction: column;
   align-items: center;
   width: 100%;
+  height: ${props => props.height || null};
 `;
 
 // Here you can use the border attribute dynamically
@@ -29,6 +30,24 @@ export const Forms = styled.form`
 */
 
 export const Input = styled.input`
+  background: #fffefe;
+  border: ${props => props.border || '1px solid rgba(0, 0, 0, 0.3)'};
+  box-sizing: border-box;
+  border-radius: 3px;
+  width: 100%;
+  color: ${c.DARK_NEUTRAL_COLOR};
+  font-family: ${f.APP_FONT};
+  font-weight: ${f.REGULAR};
+  font-size: ${f.H3_FONT_SIZE}em;
+  line-height: ${f.H3_LINE_HEIGHT}em;
+  padding: 0.5em;
+  margin: 0em 0em 1em 0em;
+  &:focus {
+    outline: 2px solid ${c.SECONDARY_POP_COLOR};
+  }
+`;
+
+export const Select = styled.select`
   background: #fffefe;
   border: ${props => props.border || '1px solid rgba(0, 0, 0, 0.3)'};
   box-sizing: border-box;
