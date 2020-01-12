2e8ae6f3fd34a74f69eadbe3e1df7a420ae4807c
Omulosi <mulongojohnpaul@gmail.com>
2020-01-12 16:43:07 +0300

    Add an effect to fetch profile data

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,16 +1,29 @@
-import React from 'react'; // import { Sidebar } from './Styles';
+import React, { useEffect } from 'react'; // import { Sidebar } from './Styles';
+import { connect } from 'react-redux';
+
+import { fetchProfileData } from '../../modules/user/userActions';
 import ProfileSidebar from '../../components/ProfileSidebar/ProfileSidebar';
 import ProfileMainPage from '../../components/ProfileMainPage/ProfileMainPage';
 
 import './styles.css';
 
-const Profile = () => {
+const Profile = ({ user, fetchProfileData }) => {
+  const profileData = user.credentials;
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
+
   return (
     <div className="profile-container">
-      <ProfileSidebar />
-      <ProfileMainPage />
+      <ProfileSidebar profileData={profileData} />
+      <ProfileMainPage profileData={profileData} />
     </div>
   );
 };
 
-export default Profile;
+export default connect(
+  state => ({
+    user: state.user,
+  }),
+  { fetchProfileData },
+)(Profile);
