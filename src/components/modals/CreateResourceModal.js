b3accbe5785288c86702dc58d23d65c0fe497ad0
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-13 00:37:34 +0100

    :sparkles: Add close button to CreateResourceModal

--- src/components/modals/CreateResourceModal.js
+++ src/components/modals/CreateResourceModal.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { MdClose } from 'react-icons/md';
+import styled from 'styled-components';
 
 import { StyledModal, ModalInner } from './modalStyles';
+import { H2 } from '../../styles/typography';
+import { WHITE } from '../../styles/variables/colours';
 
 export default function FancyModal(props) {
   const { isOpen, afterOpen, toggleModal, children } = props;
@@ -11,7 +15,19 @@ export default function FancyModal(props) {
       onBackgroundClick={toggleModal}
       onEscapeKeydown={toggleModal}
     >
+      <FancyH2 onClick={toggleModal}>
+        <MdClose size="1.6em" color={WHITE} />
+      </FancyH2>
       <ModalInner>{children}</ModalInner>
     </StyledModal>
   );
 }
+
+const FancyH2 = styled(H2)`
+  align-self: flex-start;
+  margin-left: 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
