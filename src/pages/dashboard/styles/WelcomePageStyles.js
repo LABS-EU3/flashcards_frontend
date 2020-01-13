77c34bb115101281af6ac587c3f5c4806744efe5
Oluwajoba <oluwajobabello@gmail.com>
2020-01-13 18:51:41 +0100

    add css style components / WelcomePageStyles.js

---
+++ src/pages/dashboard/styles/WelcomePageStyles.js
@@ -0,0 +1,42 @@
+//
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  /* width: auto; */
+  /* height: 100%; */
+  margin: auto 0;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-left: 2%;
+`;
+
+export const Sub1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+  margin-left: 5%;
+`;
+
+export const Sub2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 2%;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 10%;
+`;
