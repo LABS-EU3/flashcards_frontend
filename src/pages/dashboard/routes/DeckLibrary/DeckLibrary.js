4c14cc4a1887caccda36fd43ce06729b95f5505f
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-13 17:16:11 +0100

    :bug: Remove AddCard Modal from DeckLibrary

--- src/pages/dashboard/routes/DeckLibrary/DeckLibrary.js
+++ src/pages/dashboard/routes/DeckLibrary/DeckLibrary.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 
 import AddDeckForm from '../../../../components/addDeckForm/AddDeckForm';
-import AddCardForm from '../../../../components/addCardForm/AddCardForm';
 import DecksSection from './components/DecksSection';
 import TopComponent from './components/TopComponent';
 import {
@@ -29,7 +28,7 @@ const DeckLibrary = props => {
   const { dashboard } = props;
   // eslint-disable-next-line react/destructuring-assignment
   const fetchDecks = props.fetchUserDecks;
-  const { creatingDeck, creatingCard, userDecks } = dashboard;
+  const { creatingDeck, userDecks } = dashboard;
 
   const [opacity, setOpacity] = useState(0);
 
@@ -40,14 +39,6 @@ const DeckLibrary = props => {
       dispatch({ type: types.ON_DECK_CREATION_CANCELLED });
     } else {
       dispatch({ type: types.ON_START_CREATING_DECK });
-    }
-  }
-
-  function toggleCardModal() {
-    if (creatingCard) {
-      dispatch({ type: types.ON_CARD_CREATION_CANCELLED });
-    } else {
-      dispatch({ type: types.ON_START_CREATING_CARD });
     }
   }
 
@@ -74,15 +65,6 @@ const DeckLibrary = props => {
         <AddDeckForm tags={tags} />
       </FancyModal>
 
-      <FancyModal
-        isOpen={creatingCard}
-        afterOpen={afterOpen}
-        toggleModal={toggleCardModal}
-        opacity={opacity}
-        backgroundProps={{ opacity }}
-      >
-        <AddCardForm tags={tags} />
-      </FancyModal>
       <DecksSection decks={userDecks} />
     </div>
   );
