0d635e71ef6919d745869b34af6a28d91b47c93a
Oluwajoba <oluwajobabello@gmail.com>
2020-01-14 04:42:01 +0100

    update styles / WelcomePageStyles.js

--- src/pages/dashboard/styles/WelcomePageStyles.js
+++ src/pages/dashboard/styles/WelcomePageStyles.js
@@ -1,42 +1,80 @@
-//
 import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  background-color: transparent;
+  width: 100%;
+  height: 600px;
+  perspective: 1000px;
+`;
+
+export const FlipCardInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+`;
+
+export const FlipCardFront = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  background-color: #bbb;
+  color: black;
+  transform: rotateY(180deg);
+`;
+export const FlipCardBack = styled.div`
+  position: absolute;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
-  /* width: auto; */
-  /* height: 100%; */
-  margin: auto 0;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  color: black;
 `;
 
 export const Main = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
-  margin-left: 2%;
+  justify-content: center;
+  color: black;
+  background-color: white;
+  border-radius: 5px;
 `;
-
-export const Sub1 = styled.div`
+export const Main1 = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 0;
-  margin-left: 5%;
+  align-items: flex-start;
+  justify-content: center;
+  text-align: left;
+  padding: 1%;
+  margin: 2%;
 `;
-
-export const Sub2 = styled.div`
+export const Main2 = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  margin-top: 2%;
+  align-items: flex-end;
+  justify-content: center;
+  text-align: right;
+  padding: 1%;
+  margin: 2%;
 `;
 
 export const Card = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
+  align-content: center;
+  justify-content: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  width: 50%;
+  height: 100px;
   margin-top: 10%;
+  background-color: white;
+  border-radius: 5px;
 `;
