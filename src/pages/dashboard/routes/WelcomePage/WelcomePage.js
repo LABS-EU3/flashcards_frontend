25285789570022f524a1d9ba9a7414719a108768
Oluwajoba <oluwajobabello@gmail.com>
2020-01-14 04:40:32 +0100

    update WelcomePage style variables / WelcomePage.js

--- src/pages/dashboard/routes/WelcomePage/WelcomePage.js
+++ src/pages/dashboard/routes/WelcomePage/WelcomePage.js
@@ -1,37 +1,51 @@
 import React from 'react';
-import { H1, H2, HR, P } from '../../../../styles/typography';
+import { H1, H2, P } from '../../../../styles/typography';
 import {
   Container,
+  FlipCardInner,
+  FlipCardFront,
+  FlipCardBack,
   Main,
-  Sub1,
-  Sub2,
+  Main1,
+  Main2,
   Card,
 } from '../../styles/WelcomePageStyles';
 
 const WelcomePage = () => {
   return (
     <Container>
-      <Main>
-        <H1>Release Notes: 0.0.1</H1>
-        <Sub1>
-          <P>
-            {' '}
-            - Users are now able to create their account and log into the app
-          </P>
-          <P> - Create, edit, and delete decks </P>
-          <P> - Access their dashboard</P> <P>- View their profile </P>
-          <P> - View the card of the day</P>
-          <Sub2>
-            <P>Thank you for checking us out!</P>
-            <P>- The Quick Decks Team</P>
-          </Sub2>
-        </Sub1>
-        <HR />
-        <Card>
-          <H2>Card of the Day</H2>
-          <P>What is the formula that you use to find that you</P>
-        </Card>
-      </Main>
+      <FlipCardInner>
+        <FlipCardFront>
+          <img src="img_avatar.png" alt="Avatar" />
+        </FlipCardFront>
+
+        <FlipCardBack>
+          <Main>
+            <Main1>
+              <H1>Release Notes: 0.0.1</H1>
+              <P>
+                - Users are now able to create their account and log into the
+                app
+              </P>
+              <P>- Create, edit, and delete decks</P>
+              <P>
+                - Access their dashboard- View their profile- View the card of
+                the day
+              </P>
+              <P>- View their profile- View the card of the day</P>
+              <P>- View the card of the day</P>
+            </Main1>
+            <Main2>
+              <P>Thank you for checking us out!</P>
+              <P>- The Quick Decks Team</P>
+            </Main2>
+          </Main>
+          <Card>
+            <H2>Card of the Day</H2>
+            <P>What is the formula that you use to find that you</P>
+          </Card>
+        </FlipCardBack>
+      </FlipCardInner>
     </Container>
   );
 };
