7af9aeee43c928365b48021cb94e6fb7df323f29
Oluwajoba <oluwajobabello@gmail.com>
2020-01-16 08:07:53 +0100

    make side  bar open and docked / DashboardLayout.js

--- src/pages/dashboard/DashboardLayout.js
+++ src/pages/dashboard/DashboardLayout.js
@@ -1,14 +1,15 @@
 // Import
 
 // Libraries
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from 'react-sidebar';
 // slcie data - render only 5 cards
+import { useSelector } from 'react-redux';
 
 import * as g from '../../styles/variables/global';
 import LeftSideBar from '../../components/leftSideBar/LeftSideBar';
 import {
-  HamburgerButton,
+  // HamburgerButton,
   DashboardContainer,
   sideBarStyle,
   sideBarRootStyle,
@@ -16,9 +17,24 @@ import {
 } from './styles/DashboardStyles';
 
 export default function DashboardLayout(props) {
-  const { user, children, logoutUser } = props;
+  // export function DashboardLayout(props) {
+  const {
+    // user,
+    children,
+    // logoutUser
+  } = props;
+  const joba1 = useSelector(state => state.dashboard);
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  // const handleClose = reason => {
+  //   if (sidebarOpen === joba1.showMenu) {
+  //     return;
+  //     // return 'hurayyyyyy';
+  //   } else return setSidebarOpen(!sidebarOpen);
+  //   // return 'oh nooooo';
+  // };
+
+  // console.log('dddd', props);
   const mql = window.matchMedia(`(min-width: ${g.desktopMediaBreak}px)`);
 
   const [sideBarDocked, setSideBarDocked] = useState(mql.matches);
@@ -30,13 +46,31 @@ export default function DashboardLayout(props) {
 
   mql.addListener(mediaQueryChanged);
 
+  // const dispatch = useDispatch();
+  // const newFun = () => dispatch(setSidebarOpen(!sidebarOpen));
+
+  // function sayHello() {
+  //   return 'Hello';
+  // }
+  // const sendSomething = () =>
+  //   dispatch({ type: 'SAYHELLO', payload: 'yyyyyyyy' });
+  // setSidebarOpen(() => joba1.showMenu);
+
+  useEffect(() => {
+    // console.log('render');
+    setSidebarOpen(!sidebarOpen);
+  }, [joba1.showMenu]);
+
   return (
     <DashboardContainer>
+      {/* {console.log('///ooottt', joba1.showMenu)} */}
+      {/* {console.log('%%%%%%', handleClose())} */}
+
       <Sidebar
         sidebar={
           <LeftSideBar
-            name={user.credentials.full_name}
-            logoutUser={logoutUser}
+          // name={user.credentials.full_name}
+          // logoutUser={logoutUser}
           />
         }
         open={sidebarOpen}
@@ -48,16 +82,25 @@ export default function DashboardLayout(props) {
         }}
       >
         <MainContent>
-          <HamburgerButton
+          {/* <HamburgerButton
             isDocked={sideBarDocked}
             onClick={() => setSidebarOpen(!sidebarOpen)}
           >
             Open sidebar
-          </HamburgerButton>
-
+          </HamburgerButton> */}
+          {sideBarDocked}
           {children}
         </MainContent>
       </Sidebar>
     </DashboardContainer>
   );
 }
+
+// const mapStateToProps = state => {
+//   return {
+//     jjjj: state.dashboard,
+//     tttt: sayHello,
+//   };
+// };
+
+// export default connect(mapStateToProps, { sayHello })(DashboardLayout);
