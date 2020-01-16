ccb78eadbc79ffb0eb56591c1a350f4ed13a35de
Anna Winther <anski.anna@gmail.com>
2020-01-16 08:46:54 +0000

    Initial commit, add edit icon on a single card

--- src/pages/dashboard/routes/SingleDeck/cardsSection/CardsSection.js
+++ src/pages/dashboard/routes/SingleDeck/cardsSection/CardsSection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdEdit } from 'react-icons/md';
 import { H1, HR, H2, P } from '../../../../../styles/typography';
 import { CardsFlex } from '../../../../../components/cards/Cards';
-
 import {
   Collection,
   DecksContainer,
   CollectionLabel,
   IconWithoutText,
+  LibraryActions,
 } from '../../../styles/DeckLibraryStyles';
 
 const Decks = ({ cards, deleteCard }) => {
@@ -30,15 +30,22 @@ const Decks = ({ cards, deleteCard }) => {
               <CardsFlex width="46%" marginLeft="0" marginRight="0">
                 <H2 BOLD>{card.question}</H2>
                 <P>{card.answer}</P>
-                <IconWithoutText
-                  onClick={() => {
-                    handleDelete(card);
-                  }}
-                >
-                  <H2>
-                    <MdDelete />
-                  </H2>
-                </IconWithoutText>
+                <LibraryActions>
+                  <IconWithoutText
+                    onClick={() => {
+                      handleDelete(card);
+                    }}
+                  >
+                    <H2>
+                      <MdDelete />
+                    </H2>
+                  </IconWithoutText>
+                  <IconWithoutText>
+                    <H2>
+                      <MdEdit />
+                    </H2>
+                  </IconWithoutText>
+                </LibraryActions>
               </CardsFlex>
             );
           })}
