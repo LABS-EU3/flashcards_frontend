08e4b640d7abd8e7960c2d524587323ed7337ab1
Oluwajoba <oluwajobabello@gmail.com>
2020-01-17 12:12:55 +0100

    add hr tag and div / WelcomePage.js

--- src/pages/dashboard/routes/WelcomePage/WelcomePage.js
+++ src/pages/dashboard/routes/WelcomePage/WelcomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { H1, H2, P } from '../../../../styles/typography';
+import { H1, H2, P, HR } from '../../../../styles/typography';
 import {
   Container,
   FlipCardInner,
@@ -9,6 +9,7 @@ import {
   Main1,
   Main2,
   Card,
+  HRDiv,
 } from '../../styles/WelcomePageStyles';
 
 const WelcomePage = () => {
@@ -36,11 +37,15 @@ const WelcomePage = () => {
               <P>- View their profile- View the card of the day</P>
               <P>- View the card of the day</P>
             </Main1>
+
             <Main2>
               <P>Thank you for checking us out!</P>
               <P>- The Quick Decks Team</P>
             </Main2>
           </Main>
+          <HRDiv>
+            <HR />
+          </HRDiv>
           <Card>
             <H2>Card of the Day</H2>
             <P>What is the formula that you use to find that you</P>
