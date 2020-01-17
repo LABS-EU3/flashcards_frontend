33b1ee082dfc2473602b2e227490537f72214e89
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-17 17:17:50 +0100

    :bug: Fix console warning: children have same key

--- src/pages/dashboard/routes/DeckLibrary/components/DecksSection.js
+++ src/pages/dashboard/routes/DeckLibrary/components/DecksSection.js
@@ -27,7 +27,7 @@ const Decks = ({ decks }) => {
               onClick={() => {
                 dispatch({ type: types.ON_SELECT_DECK, payload: { ...d } });
               }}
-              key={d.deck_name}
+              key={d.deck_id}
               width="46%"
               marginLeft="0"
               marginRight="0"
