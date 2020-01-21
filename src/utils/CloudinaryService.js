7e174eab8e7eeefad7f379e12b5f574bada49587
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-21 17:25:49 +0100

    :bug: Accept imageTags in upload function

--- src/utils/CloudinaryService.js
+++ src/utils/CloudinaryService.js
@@ -1,7 +1,12 @@
 import { Util } from 'cloudinary-core';
 
 // eslint-disable-next-line import/prefer-default-export
-export const openUploadWidget = (options, callback) => {
+export const openUploadWidget = (imageTags, callback) => {
+  const options = {
+    cloudName: process.env.cloudName,
+    uploadPreset: process.env.cloudinaryPresetName,
+    tags: imageTags,
+  };
   const scOptions = Util.withSnakeCaseKeys(options);
   window.cloudinary.openUploadWidget(scOptions, callback);
 };
