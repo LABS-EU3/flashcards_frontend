25c5ca5473ce2458deed6e0a2098fa5faaef663a
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-21 18:13:16 +0100

    :bug: Use string literals instead of .env on upload
    Also add check for when image tags empty

--- src/utils/CloudinaryService.js
+++ src/utils/CloudinaryService.js
@@ -2,10 +2,11 @@ import { Util } from 'cloudinary-core';
 
 // eslint-disable-next-line import/prefer-default-export
 export const openUploadWidget = (imageTags, callback) => {
+  const tags = imageTags.length > 0 ? imageTags : [];
   const options = {
-    cloudName: process.env.cloudName,
-    uploadPreset: process.env.cloudinaryPresetName,
-    tags: imageTags,
+    cloudName: 'quick-decks',
+    uploadPreset: 'upload',
+    tags,
   };
   const scOptions = Util.withSnakeCaseKeys(options);
   window.cloudinary.openUploadWidget(scOptions, callback);
