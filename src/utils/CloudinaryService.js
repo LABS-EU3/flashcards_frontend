20ef1e7874358cdc381a14f757d9024d2d920937
Maaruf Dauda <mkdauda.work@gmail.com>
2020-01-21 18:42:33 +0100

    :sparkles: Add response callbacks to upload function

--- src/utils/CloudinaryService.js
+++ src/utils/CloudinaryService.js
@@ -1,7 +1,11 @@
 import { Util } from 'cloudinary-core';
 
 // eslint-disable-next-line import/prefer-default-export
-export const openUploadWidget = (imageTags, callback) => {
+export const openUploadWidget = (
+  imageTags,
+  onUploadFailedCallback,
+  onUploadSuccessCallback,
+) => {
   const tags = imageTags.length > 0 ? imageTags : [];
   const options = {
     cloudName: 'quick-decks',
@@ -9,5 +13,16 @@ export const openUploadWidget = (imageTags, callback) => {
     tags,
   };
   const scOptions = Util.withSnakeCaseKeys(options);
-  window.cloudinary.openUploadWidget(scOptions, callback);
+  window.cloudinary.openUploadWidget(scOptions, (error, photos) => {
+    if (!error) {
+      if (photos.event === 'success' && onUploadSuccessCallback) {
+        // eslint-disable-next-line camelcase
+        const { secure_url } = photos.info;
+        onUploadSuccessCallback(secure_url);
+      }
+    } else {
+      const errorMessage = error.status ? error.status : 'Image upload failed';
+      if (onUploadFailedCallback) onUploadFailedCallback(errorMessage);
+    }
+  });
 };
