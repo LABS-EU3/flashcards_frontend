3ae6d855c7e58c84b24790fdabbf74a0dd497556
Oluwajoba <oluwajobabello@gmail.com>
2020-01-24 09:23:53 +0100

    make top and bottom components  mobile responsive / StudyMode.js

--- src/pages/dashboard/routes/StudyMode/StudyMode.js
+++ src/pages/dashboard/routes/StudyMode/StudyMode.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+
 import styled from 'styled-components';
 import './studymode.css';
-
 import ReactSearchBox from 'react-search-box';
-
-import { MdCollectionsBookmark } from 'react-icons/md';
+import { MdCollectionsBookmark, MdKeyboardArrowDown } from 'react-icons/md';
 import { Line } from 'rc-progress';
 import { H1, H2, H3, P } from '../../../../styles/typography';
 
@@ -13,14 +12,18 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* justify-content: center; */
 `;
 
 export const TopContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
+
   margin-top: 30px;
+  @media (max-width: 900px) {
+    width: 80%;
+    flex-direction: column;
+  }
 `;
 
 export const BottomContainer = styled.div`
@@ -29,6 +32,9 @@ export const BottomContainer = styled.div`
   justify-content: space-around;
   margin-top: 40px;
   width: 95%;
+  @media (min-width: 1500px) {
+    width: 75%;
+  }
   @media (max-width: 900px) {
     width: 80%;
     flex-direction: column;
@@ -48,26 +54,27 @@ export const Card = styled.div`
   padding-bottom: 2%;
   background-color: white;
   border-radius: 5px;
+`;
 
-  /* @media (max-width: 900px) {
-    width: 600px;
-    flex-direction: row;
-  } */
+export const UpperCardSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const SLowerCardSection = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  /* margin-left: -7%; */
 `;
+
 export const SLower = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   width: 40%;
-  /* margin-left: -7%; */
 `;
+
 export const MLower = styled.div`
   display: flex;
   align-items: center;
@@ -76,16 +83,36 @@ export const MLower = styled.div`
   margin-top: -4%;
 `;
 
+export const RecentlyViewContainer = styled.div`
+  margin: 10px 10px 10px 20px;
+  width: 100%;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export const SessionContainer = styled.div`
   align-self: flex-start;
-  margin: 10px;
+  margin: 10px 10px 10px 5px;
   width: 100%;
 `;
+
 export const MasteryContainer = styled.div`
   margin: 10px 10px 10px 20px;
   width: 100%;
 `;
-export const CardContainer = styled.div``;
+
+export const CardContainer = styled.div`
+  /* display: none; */
+`;
+
+const StyledMyPart = styled(CardContainer).attrs({
+  className: 'container',
+})`
+  &.container {
+    /* display: none; */
+  }
+`;
 
 export const BUTTON = styled.button`
   height: 40px;
@@ -106,6 +133,7 @@ export const MyHR = styled.hr`
     rgba(255, 169, 135, 0.5) 136.86%
   );
 `;
+
 const StyledLink = styled(NavLink)`
   text-decoration: none;
 `;
@@ -146,6 +174,41 @@ const mastery = [
 ];
 
 export default function StudyMode() {
+  const container = React.createRef();
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
+  // const handleClickOutside = event => {
+  //   if (container.current && !container.current.contains(event.target)) {
+  //     setOpen(false);
+  //   }
+  // };
+
+  const handleButtonClick1 = () => {
+    setOpen1(!open1);
+  };
+  const handleButtonClick2 = () => {
+    setOpen2(!open2);
+  };
+  // document.addEventListener('mousedown', handleClickOutside);
+  // document.removeEventListener('mousedown', handleClickOutside);
+
+  const mql = window.matchMedia(`(max-width: 768px)`);
+  let resizeTimeout;
+  window.addEventListener('resize', function() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(function() {
+      window.location.reload();
+    }, 1500);
+  });
+
+  useEffect(() => {
+    /* eslint-disable-next-line no-unused-expressions */
+    !mql.matches ? setOpen1(!open1) : setOpen1(open1);
+    /* eslint-disable-next-line no-unused-expressions */
+    !mql.matches ? setOpen2(!open2) : setOpen2(open2);
+  }, [mql.matches]);
+
   return (
     <Wrapper>
       <TopContainer>
@@ -169,56 +232,88 @@ export default function StudyMode() {
           </StyledLink>
         </BUTTON>
       </TopContainer>
-
       <BottomContainer>
+        <RecentlyViewContainer>
+          <UpperCardSection>
+            <H1 lineHeight="1em">Jump back into</H1>
+            <MdKeyboardArrowDown
+              size="4em"
+              color="grey"
+              // onClick={handleButtonClick1}
+              className="material-icons"
+            />
+          </UpperCardSection>
+          <MyHR />
+        </RecentlyViewContainer>
+
         <SessionContainer>
-          <H1 lineHeight="1em">Session</H1>
+          <UpperCardSection>
+            <H1 lineHeight="1em">Session</H1>
+            <MdKeyboardArrowDown
+              size="4em"
+              color="grey"
+              onClick={handleButtonClick1}
+              className="material-icons"
+            />
+          </UpperCardSection>
           <MyHR />
 
-          <CardContainer>
-            {sessions.map((data, index) => {
-              return (
-                /* eslint-disable-next-line react/no-array-index-key */
-                <Card key={index}>
-                  <H2>{data.cardTitle}</H2>
-                  <SLowerCardSection>
-                    <P>{data.mode} mode</P>
-                    <SLower>
-                      <P color="grey">{data.totalCard} Cards left</P>
-                      <MdCollectionsBookmark size="2em" color="grey" />
-                    </SLower>
-                  </SLowerCardSection>
-                </Card>
-              );
-            })}
+          <CardContainer className="container" ref={container}>
+            <StyledMyPart>
+              {open1 &&
+                sessions.map((data, index) => {
+                  return (
+                    /* eslint-disable-next-line react/no-array-index-key */
+                    <Card key={index}>
+                      <H2>{data.cardTitle}</H2>
+                      <SLowerCardSection>
+                        <P>{data.mode} mode</P>
+                        <SLower>
+                          <P color="grey">{data.totalCard} Cards left</P>
+                          <MdCollectionsBookmark size="2em" color="grey" />
+                        </SLower>
+                      </SLowerCardSection>
+                    </Card>
+                  );
+                })}
+            </StyledMyPart>
           </CardContainer>
         </SessionContainer>
 
         <MasteryContainer>
-          <H1 lineHeight="1em"> Mastery</H1>
+          <UpperCardSection>
+            <H1 lineHeight="1em">Mastery</H1>
+            <MdKeyboardArrowDown
+              size="4em"
+              color="grey"
+              onClick={handleButtonClick2}
+              className="material-icons"
+            />
+          </UpperCardSection>
           <MyHR />
 
-          <CardContainer>
-            {mastery.map((data, index) => {
-              return (
-                /* eslint-disable-next-line react/no-array-index-key */
-                <Card key={index}>
-                  <H2>{data.cardTitle}</H2>
-                  <MLower>
-                    <Line
-                      percent={data.percent}
-                      strokeLineColor="red"
-                      strokeWidth="4"
-                      trailWidth="4"
-                      trailColor="#fafafa"
-                      strokeColor="#8D99AE"
-                      className="pBar"
-                    />
-                    <H2>{data.percent} %</H2>
-                  </MLower>
-                </Card>
-              );
-            })}
+          <CardContainer className="container" ref={container}>
+            {open2 &&
+              mastery.map((data, index) => {
+                return (
+                  /* eslint-disable-next-line react/no-array-index-key */
+                  <Card key={index}>
+                    <H2>{data.cardTitle}</H2>
+                    <MLower>
+                      <Line
+                        percent={data.percent}
+                        strokeLineColor="red"
+                        strokeWidth="4"
+                        trailWidth="4"
+                        trailColor="#fafafa"
+                        strokeColor="#8D99AE"
+                        className="pBar"
+                      />
+                      <H2>{data.percent} %</H2>
+                    </MLower>
+                  </Card>
+                );
+              })}
           </CardContainer>
         </MasteryContainer>
       </BottomContainer>
