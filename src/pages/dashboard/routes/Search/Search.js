b7f04d6ceb1a4d99af27aa046391d28dee8ba987
richanynguon <alina.nguon@gmail.com>
2020-01-27 06:25:38 -0500

    add back button to search page

--- src/pages/dashboard/routes/Search/Search.js
+++ src/pages/dashboard/routes/Search/Search.js
@@ -1,8 +1,34 @@
+// Imports
+// Libraries
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+
+// Styles
+import { HR, H5 } from '../../../../styles/typography';
+import { BackArrowButton } from '../../../../styles/buttons';
+
+// Actions
+import { CLEAR_RESPONSES } from '../../../../modules/user/userTypes';
+
+// Assets
+import BackArrow from '../../../../assets/icons/Arrow 1.svg';
 
 export default function Search() {
+  const history = useHistory();
+  const dispatch = useDispatch();
   return (
     <div>
+      <BackArrowButton
+        onClick={() => {
+          dispatch({ type: CLEAR_RESPONSES });
+          history.goBack();
+        }}
+      >
+        <img src={`${BackArrow}`} alt="back arrow" />
+        <H5>Back</H5>
+      </BackArrowButton>
+      <HR />
       <h1>Search</h1>
     </div>
   );
