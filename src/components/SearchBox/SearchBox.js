1073ce4a06a7bb62a66f60ab5958591e9e72d77f
richanynguon <alina.nguon@gmail.com>
2020-01-27 06:03:05 -0500

    add search  bar to link to the search page

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import { FaSearch } from 'react-icons/fa';
 import { Input, NavSearch, Search } from './styles';
 
 const SearchBox = () => {
+  const history = useHistory();
   return (
     <Search>
       <NavSearch>
         <Input type="text" name="q" placeholder="Find a deck" />
       </NavSearch>
-      <button>
+      <button
+        type="button"
+        onClick={() => {
+          history.push('/dashboard/search');
+        }}
+      >
         <FaSearch size={15} />
       </button>
     </Search>
